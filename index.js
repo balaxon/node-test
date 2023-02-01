ebf51180095d64ee6024f8c2d8146e6ded7ba9b0
balaxon <balahanov2209@gmail.com>
2023-02-01 17:57:58 +0300

    no-commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ if (window.localStorage.getItem('href') !== '') {
   console.log('tut');
   history.pushState(null, null, window.localStorage.getItem('href'));
   const main = document.createElement('main');
+  main.innerHTML = `<h1>${window.localStorage.getItem('href')}</h1>`;
   document.body.append(main);
   window.localStorage.setItem('href', '');
   
