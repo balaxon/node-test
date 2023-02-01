1c8805ee475c2765f64d70fedeca7dc2db30da0a
balaxon <balahanov2209@gmail.com>
2023-02-01 17:47:33 +0300

    no-commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ if (window.localStorage.getItem('href') !== '') {
   console.log('tut');
   history.pushState(null, null, window.localStorage.getItem('href'));
   window.localStorage.setItem('href', '');
+  document.body.innerHTML = window.localStorage.getItem('href');
 } else {
   alert('мы на главной')
 }
