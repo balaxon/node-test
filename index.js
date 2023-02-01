2aaba668fa17e5c5e5316f4c98d77e0416a5cfc2
balaxon <balahanov2209@gmail.com>
2023-02-01 14:47:18 +0300

    no-commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 if (window.localStorage.getItem('from404') === 'true') {
   console.log('tut');
   window.localStorage.setItem('from404', 'false');
-  history.pushState(window.localStorage.getItem('href'));
+  history.pushState(null, null, window.localStorage.getItem('href'));
 } else {
   alert('мы на главной')
 }
