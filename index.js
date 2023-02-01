6eeb490acbc493591c3768d13018388d06e4bebb
balaxon <balahanov2209@gmail.com>
2023-02-01 19:03:30 +0300

    no-commit

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ document.getElementsByTagName('ul')[0].children[3].addEventListener('click', () 
   window.location.href = 'https://balaxon.github.io/node-test/multi-game';
 });
 
-window.onpopstate = () => {
-  alert('popstate');
-};
-
 if (url !== '') {
   console.log('tut');
   history.pushState(null, null, url);
@@ -30,30 +26,26 @@ if (url !== '') {
   alert('мы на главной');
 }
 
-window.addEventListener('popstate', () => {
-  switch (window.localStorage.setItem('href', window.location.href.replace(window.location.origin, ''))) {
-    case '/node-test/shop':
-      alert('вызываем shop');
-      break;
-  
-      case '/node-test/single-game':
-        alert('вызываем single-game');
-        break;
-  
-      case '/node-test/multi-game':
-        alert('вызываем multi-game');
-        break;
-  
-      case '':
-        alert('вызываем main page');
-        break;
-  
-    default:
-      alert('вызываем 404 page');
-      break;
-  }
-});
+switch (window.localStorage.setItem('href', window.location.href.replace(window.location.origin, ''))) {
+  case '/node-test/shop':
+    alert('вызываем shop');
+    break;
 
+    case '/node-test/single-game':
+      alert('вызываем single-game');
+      break;
 
+    case '/node-test/multi-game':
+      alert('вызываем multi-game');
+      break;
+
+    case '':
+      alert('вызываем main page');
+      break;
+
+  default:
+    alert('вызываем 404 page');
+    break;
+}
 
 console.log('index');
